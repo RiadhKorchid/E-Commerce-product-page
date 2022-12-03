b651cab1699c19c60e857dda3f9107d3ae6a92ff
Riadh Korchid <riadhkourchid@gmail.com>
2022-12-04 00:00:17 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav"
+import "../styles/app.scss"
+export default function (){
+    return (
+        <>
+        <Nav/>
+        </>
+    )
+}
+
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../images/logo.svg"
+import  iconCart from "../images/icon-cart.svg"
+import avatar from "../images/image-avatar.png"
+export default function ()
+ {
+    return (
+        <nav>
+<div className="nav-left">
+        <img src={logo} alt="" />
+            <ul>
+    <li>Collections</li>
+    <li>Men</li>
+    <li>Women</li>
+    <li>About</li>
+    <li>Contact</li>
+            </ul>
+</div>
+<div className="nav-right">
+<img src={iconCart} alt="" />
+<img src={avatar} alt="" />
+</div>
+</nav>
+    )
+ }
